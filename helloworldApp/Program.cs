efdb805417972f2c9d8cc4803df363d33173e84f
vivek0623 <vivekbangera44@gmail.com>
2022-07-12 17:39:03 +0530

    added line 2 and third

--- helloworldApp/Program.cs
+++ helloworldApp/Program.cs
@@ -7,6 +7,8 @@ namespace helloworldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is Line");
+            Console.WriteLine("Welcome to Right");
         }
     }
 }
